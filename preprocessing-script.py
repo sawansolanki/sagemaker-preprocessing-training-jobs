e11d13dfd5d98662d1783263df04fc78f06ac816
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-28 13:00:46 +0530

    Update preprocessing-script.py

--- preprocessing-script.py
+++ preprocessing-script.py
@@ -2,6 +2,9 @@ import boto3
 import sagemaker
 from sagemaker import get_execution_role
 from sagemaker.sklearn.processing import SKLearnProcessor
+from sagemaker.processing import ProcessingInput, ProcessingOutput
+
+
 
 region = boto3.session.Session().region_name
 
@@ -9,4 +12,17 @@ role = get_execution_role()
 sklearn_processor = SKLearnProcessor(
     framework_version="0.20.0", role=role, instance_type="ml.m5.xlarge", instance_count=1
 )
+
+if __name__ == "__main__":
+
+    sklearn_processor.run(
+        code="preprocessing.py",
+        inputs=[ProcessingInput(source=input_data, destination="/opt/ml/processing/input")],
+        outputs=[
+            ProcessingOutput(output_name="train_data", source="/opt/ml/processing/train"),
+            ProcessingOutput(output_name="test_data", source="/opt/ml/processing/test"),
+        ],
+        arguments=["--train-test-split-ratio", "0.2"],
+    )
+
 
