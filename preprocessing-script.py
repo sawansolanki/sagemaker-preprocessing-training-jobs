9efaebf3b3f68f7d697aaae25800a69b0ae9dac5
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-28 13:18:25 +0530

    Update preprocessing-script.py

--- preprocessing-script.py
+++ preprocessing-script.py
@@ -4,16 +4,14 @@ from sagemaker import get_execution_role
 from sagemaker.sklearn.processing import SKLearnProcessor
 from sagemaker.processing import ProcessingInput, ProcessingOutput
 
-
-
-region = boto3.session.Session().region_name
-
-role = get_execution_role()
-sklearn_processor = SKLearnProcessor(
-    framework_version="0.20.0", role=role, instance_type="ml.m5.xlarge", instance_count=1
-)
-
 if __name__ == "__main__":
+    
+    role = get_execution_role()
+    region = boto3.session.Session().region_name
+    
+    sklearn_processor = SKLearnProcessor(
+    framework_version="0.20.0", role=role, instance_type="ml.m5.xlarge", instance_count=1
+    )
 
     sklearn_processor.run(
         code="preprocessing.py",
