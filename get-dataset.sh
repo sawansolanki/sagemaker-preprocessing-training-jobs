de520a1dd4bd8fef70c281b6ca1711b87b87d959
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-29 11:01:20 +0530

    Update get-dataset.sh

--- get-dataset.sh
+++ get-dataset.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 sudo apt-get update
-
-sudo apt-get install python
-pip install sagemaker --upgrade
+echo y | sudo apt-get install pip
+sudo apt-get install python3
+pip -q install sagemaker --upgrade
 sudo apt-get -y install unzip
 
 wget -N https://sagemaker-sample-data-us-west-2.s3-us-west-2.amazonaws.com/autopilot/direct_marketing/bank-additional.zip
