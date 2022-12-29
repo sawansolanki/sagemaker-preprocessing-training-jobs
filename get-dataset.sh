29e8fa5449a2beb5e05fbb97456ed548cdeefae3
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-29 11:30:25 +0530

    Update get-dataset.sh

--- get-dataset.sh
+++ get-dataset.sh
@@ -8,3 +8,9 @@ sudo apt-get -y install unzip
 
 wget -N https://sagemaker-sample-data-us-west-2.s3-us-west-2.amazonaws.com/autopilot/direct_marketing/bank-additional.zip
 unzip -o bank-additional.zip
+
+mkdir -p /opt/ml/processing/train
+mkdir -p /opt/ml/processing/test
+mkdir -p /opt/ml/processing/input
+
+cp bank-additional/bank-additional-full.csv /opt/ml/processing/input/
