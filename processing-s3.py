e69a8b0f25c57a67c543ef2b38af33be444494e7
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-29 16:46:24 +0530

    Update processing-s3.py

--- processing-s3.py
+++ processing-s3.py
@@ -79,11 +79,11 @@ if __name__=='__main__':
 
 
     
-    train_features_output_path = 's3://opt/ml/processing/train/out-data-' + datestring +  '/train_features.csv'
-    train_labels_output_path = 's3://opt/ml/processing/train/out-data'+ datestring + '/train_labels.csv'
+    train_features_output_path = 's3://put-data-slp/opt/ml/processing/train/out-data-' + datestring +  '/train_features.csv'
+    train_labels_output_path = 's3://put-data-slp/opt/ml/processing/train/out-data'+ datestring + '/train_labels.csv'
     
-    test_features_output_path = 's3://opt/ml/processing/test/out-data-' + datestring + '/test_features.csv'
-    test_labels_output_path = 's3://opt/ml/processing/test/out-data-' + datestring + '/test_labels.csv'
+    test_features_output_path = 's3://put-data-slp/opt/ml/processing/test/out-data-' + datestring + '/test_features.csv'
+    test_labels_output_path = 's3://put-data-slp/opt/ml/processing/test/out-data-' + datestring + '/test_labels.csv'
     
     print('Saving training features to {}'.format(train_features_output_path))
     pd.DataFrame(train_features).to_csv(train_features_output_path, header=False, index=False)
