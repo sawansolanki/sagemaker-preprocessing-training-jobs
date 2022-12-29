d5f5ee0b72d2511318a14d9ecd294224ff6649fb
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-29 22:12:29 +0530

    Update get-dataset.sh

--- get-dataset.sh
+++ get-dataset.sh
@@ -2,19 +2,24 @@
 
 sudo apt-get update
 sudo apt-get install python3
-sudo apt install python3-pip
-pip3 install --upgrade setuptools wheel
+sudo apt install python3-pip -y
 
 pip3 install --upgrade setuptools wheel
 python3 -m pip install s3fs --user
 pip3 install boto3
+
 python3 -m pip install boto3 pandas s3fs sagemaker --user
 #pip3 -q install sagemaker --upgrade
 sudo apt-get -y install unzip
-pip3 install scikit-learn
 
+pip3 install scikit-learn
 wget -N https://sagemaker-sample-data-us-west-2.s3-us-west-2.amazonaws.com/autopilot/direct_marketing/bank-additional.zip
 unzip -o bank-additional.zip
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt-get install less
+unzip awscliv2.zip
+sudo ./aws/install
 
 mkdir -p /opt/ml/processing/train
 mkdir -p /opt/ml/processing/test
