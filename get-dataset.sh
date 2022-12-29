0a9d9fe39b29e1ede68883402b3bcf147c912fa3
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-29 18:23:21 +0530

    Update get-dataset.sh

--- get-dataset.sh
+++ get-dataset.sh
@@ -4,7 +4,11 @@ sudo apt-get update
 sudo apt-get install python3
 sudo apt install python3-pip
 pip3 install --upgrade setuptools wheel
-python3 -m pip install boto3 pandas s3fs
+
+pip3 install --upgrade setuptools wheel
+python3 -m pip install s3fs --user
+pip3 install boto3
+#python3 -m pip install boto3 pandas s3fs --user
 pip3 -q install sagemaker --upgrade
 sudo apt-get -y install unzip
 pip3 install scikit-learn
