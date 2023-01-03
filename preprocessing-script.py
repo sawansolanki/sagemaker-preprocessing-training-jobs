f08e7f8977c4868661ceb1a31f25276366c2ab8b
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2023-01-03 12:07:08 +0530

    Update preprocessing-script.py

--- preprocessing-script.py
+++ preprocessing-script.py
@@ -1,24 +1,36 @@
-#!/usr/bin/python3
+import boto3
+import sagemaker
+from sagemaker import get_execution_role
 from sagemaker.sklearn.processing import SKLearnProcessor
-
+region = boto3.session.Session().region_name
 from sagemaker.processing import ProcessingInput, ProcessingOutput
 
-import sagemaker
+input_data = "s3://sagemaker-sample-data-{}/processing/census/census-income.csv".format(region)
 
-role = sagemaker.get_execution_role()
+def run_prepros():
+#creating processing job instance
+    role = get_execution_role()
+    sklearn_processor = SKLearnProcessor(
+        framework_version="0.20.0", role=role, instance_type="ml.c4.2xlarge", instance_count=1
+    )
 
-sklearn_processor = SKLearnProcessor(framework_version='0.23-1',
-                                     role=role,
-                                     instance_type='ml.c4.2xlarge',
-                                     instance_count=1)
+    sklearn_processor.run(
+        code="preprocessing.py",
+        inputs=[ProcessingInput(source=input_data, destination="/opt/ml/processing/input")],
+        outputs=[
+            ProcessingOutput(output_name="train_data", source="/opt/ml/processing/train"),
+            ProcessingOutput(output_name="test_data", source="/opt/ml/processing/test"),
+        ],
+        arguments=["--train-test-split-ratio", "0.2"],
+    )
+    preprocessing_job_description = sklearn_processor.jobs[-1].describe()
 
-sklearn_processor.run(code='s3b.py',
-                      inputs=[ProcessingInput(
-                        source=input_data,
-                        destination='bank-additional/bank-additional-full.csv')],
-                      outputs=[ProcessingOutput(output_name='train_data',
-                                                source='/opt/ml/processing/train/out-data2022-12-29 12:51:45'),
-                               ProcessingOutput(output_name='test_data',
-                                                source='/opt/ml/processing/test/out-data2022-12-29 12:51:45')],
-                      arguments=['--train-test-split-ratio', '0.2']
-                     )
+    output_config = preprocessing_job_description["ProcessingOutputConfig"]
+    for output in output_config["Outputs"]:
+        if output["OutputName"] == "train_data":
+            preprocessed_training_data = output["S3Output"]["S3Uri"]
+        if output["OutputName"] == "test_data":
+            preprocessed_test_data = output["S3Output"]["S3Uri"]
+    
+if __name__ == "__main__":
+    run_prepros()
