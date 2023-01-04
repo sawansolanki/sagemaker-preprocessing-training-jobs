adc640840e5b39e114afa256a867df187c8400e5
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2023-01-04 14:25:06 +0530

    Update dependency.sh

--- dependency.sh
+++ dependency.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-sudo apt-get update
-sudo apt-get -y install python3
-sudo apt install python3-pip -y
+apt-get update
+apt-get -y install python3
+apt install python3-pip -y
 
 pip3 install --upgrade setuptools wheel
 python3 -m pip install s3fs --user
@@ -10,24 +10,24 @@ pip3 install boto3
 
 python3 -m pip install boto3 pandas s3fs sagemaker --user
 #pip3 -q install sagemaker --upgrade
-sudo apt-get -y install unzip
+apt-get -y install unzip
 
 pip3 install scikit-learn
 pip3 install sklearn
 pip3 install argparse
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo apt-get install less
+apt-get install less
 unzip awscliv2.zip
-sudo ./aws/install
+./aws/install
 
-sudo apt-get install -y docker.io
-sudo service docker start
+apt-get install -y docker.io
+service docker start
 
-sudo mkdir -p /opt/ml/processing/train
-sudo mkdir -p /opt/ml/processing/test
-sudo mkdir -p /opt/ml/processing/input
-sudo mkdir -p /opt/ml/processing/input/code
+mkdir -p /opt/ml/processing/train
+mkdir -p /opt/ml/processing/test
+mkdir -p /opt/ml/processing/input
+mkdir -p /opt/ml/processing/input/code
 
 cp preprocessing.py /opt/ml/processing/input/code/
 
