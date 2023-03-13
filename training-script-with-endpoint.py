b1e23c74c544eb926afaecc20ab511803e8b1868
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2023-03-13 16:56:58 +0530

    Update training-script-with-endpoint.py

--- training-script-with-endpoint.py
+++ training-script-with-endpoint.py
@@ -29,7 +29,7 @@ bucket_name = 'sagemaker-us-east-1-256537223841'
 object_key = 'sagemaker-scikit-learn-2023-03-09-07-04-34-496/output/model.joblib'
 local_file_path = 'model.joblib'
 
-s3.Bucket(bucket_name).download_file(object_key, local_file_path)
+s3.bucket(bucket_name).download_file(object_key, local_file_path)
     
 def model_fn():
    clf = joblib.load("model.joblib")
